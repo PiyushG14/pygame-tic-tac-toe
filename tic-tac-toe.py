75f43853b12e54d9536caf075f818af8c56457f7
PiyushG14 <46423477+PiyushG14@users.noreply.github.com>
2021-04-03 23:53:09 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,6 +2,16 @@ import pygame
 
 WIDTH = 250
 background_color = (251,247,245)
+CELL = 50
+
+def drawGrid(win):
+    for i in range(0,2):
+        #window, color, start coordinate, end coordinate, width =
+        pygame.draw.line(win, (0,0,0), (2*CELL + CELL*i, CELL), (2*CELL + CELL*i , 4*CELL ), 4)
+        pygame.draw.line(win, (0,0,0), (CELL, 2*CELL + CELL*i), (4*CELL ,2*CELL + CELL*i ), 4)
+    pygame.display.update()
+    return
+
 
 def main():
     pygame.init()
@@ -9,6 +19,8 @@ def main():
     pygame.display.set_caption("Tic Tac Toe")
     win.fill(background_color)
     pygame.display.update()
+    
+    drawGrid(win)
     
     while True:
         for event in pygame.event.get():
